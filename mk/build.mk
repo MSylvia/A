831a96236b3b69ceda36748d280d9df9ee73f9c7
Matt Sylvia <msylvia@nukefile.net>
2018-01-18 12:03:49 -0500

    Update build.mk

--- mk/build.mk
+++ mk/build.mk
@@ -1,4 +1,4 @@
 
-NEEDED_C_VERSION := 1fdd17add4cc4b10de0fa85cbf13ed8640ed7908
+NEEDED_C_VERSION := d1a888b72d990f4825b481dc4330e3dac1ea9a4e
 NEEDED_C_BRANCH := master
 C_MODULE := git@github.com:msylvia/c.git
